bdd8538e9a2e273892f50d27a502672da3134102
thisisdishang <dishangkumarrana@gmail.com>
2025-05-20 18:45:45 +0530

    List out all filter names of SecurityFilterChain

--- src/main/java/com/ranacorporation/SpringSecurityPlayground/controller/AppController.java
+++ src/main/java/com/ranacorporation/SpringSecurityPlayground/controller/AppController.java
@@ -1,12 +1,26 @@
 package com.ranacorporation.SpringSecurityPlayground.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AppController {
+
+    @Autowired
+    FilterChainProxy filterChainProxy;
+
     @GetMapping("/hello")
-    public String getHello() {
-        return "Hello";
+    public List<String> getHello() {
+        List<SecurityFilterChain> securityFilterChainList = filterChainProxy.getFilterChains();
+        List<String> filterNames = securityFilterChainList.stream()
+                .flatMap(securityFilterChain -> securityFilterChain.getFilters().stream())
+                .map(filter -> filter.getClass().getSimpleName())
+                .toList();
+        return filterNames;
     }
 }
