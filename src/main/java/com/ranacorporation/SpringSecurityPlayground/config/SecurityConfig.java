f08b302c17a9c7442e0fe02f738d2366758f1e28
thisisdishang <dishangkumarrana@gmail.com>
2025-05-21 18:45:45 +0530

    Configure custom username and password with UserDetailsService and PasswordEncoder

---
+++ src/main/java/com/ranacorporation/SpringSecurityPlayground/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ranacorporation.SpringSecurityPlayground.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.builder().username("jarvis").password("iamon").build();
+        userDetailsManager.createUser(userDetails);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
