d20314c9a556daa1d5fef96f6e542c104f0f19f1
thisisdishang <dishangkumarrana@gmail.com>
2025-06-06 18:45:45 +0530

    Add CustomAuthenticationManager

--- src/main/java/com/ranacorporation/SpringSecurityPlayground/config/security/filters/CustomAuthenticationFilter.java
+++ src/main/java/com/ranacorporation/SpringSecurityPlayground/config/security/filters/CustomAuthenticationFilter.java
@@ -1,15 +1,37 @@
 package com.ranacorporation.SpringSecurityPlayground.config.security.filters;
 
+import com.ranacorporation.SpringSecurityPlayground.config.security.manager.CustomAuthenticationManager;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class CustomAuthenticationFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // CustomAuthenticationManager => CustomAuthenticationProvider => CustomUserDetailsService => ....
-        filterChain.doFilter(servletRequest, servletResponse);
+public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    CustomAuthenticationManager customAuthenticationManager;
+
+    public CustomAuthenticationFilter(CustomAuthenticationManager customAuthenticationManager) {
+        this.customAuthenticationManager = customAuthenticationManager;
     }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        /*
+        1. header: authentication
+        2. if the header values matches, then move on the next filter
+        */
+        filterChain.doFilter(request, response);
+    }
+
+//    filter call: forward, include
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // CustomAuthenticationManager => CustomAuthenticationProvider => CustomUserDetailsService => ....
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
+
 }
---
+++ src/main/java/com/ranacorporation/SpringSecurityPlayground/config/security/manager/CustomAuthenticationManager.java
@@ -0,0 +1,13 @@
+package com.ranacorporation.SpringSecurityPlayground.config.security.manager;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAuthenticationManager implements AuthenticationManager {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+}
