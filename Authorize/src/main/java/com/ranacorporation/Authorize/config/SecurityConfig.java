ccd7ec275455baa981bde928c906716928ca0f65
thisisdishang <dishangkumarrana@gmail.com>
2025-06-22 18:45:45 +0530

    Disable csrf filter chain for run POST method

--- Authorize/src/main/java/com/ranacorporation/Authorize/config/SecurityConfig.java
+++ Authorize/src/main/java/com/ranacorporation/Authorize/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/admin/**").hasAuthority("READ")
                         .requestMatchers(HttpMethod.POST, "/admin/**").hasAuthority("WRITE")
                         .anyRequest().authenticated())
+                .csrf(customizer -> customizer.disable()) // do not do this in production
                 .build();
     }
 }
