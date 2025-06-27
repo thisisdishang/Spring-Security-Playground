f0db55227459e2eaf2c1e352a1275d80bf7ab9c4
thisisdishang <dishangkumarrana@gmail.com>
2025-06-27 18:45:45 +0530

    Update authorization validation function

--- Authorize/src/main/java/com/ranacorporation/Authorize/utils/AuthorizationApply.java
+++ Authorize/src/main/java/com/ranacorporation/Authorize/utils/AuthorizationApply.java
@@ -1,10 +1,12 @@
 package com.ranacorporation.Authorize.utils;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorizationApply {
-    public boolean validation(){
-        return false;
+    public boolean validation() {
+        var a = SecurityContextHolder.getContext().getAuthentication();
+        return a.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals("READ"));
     }
 }
