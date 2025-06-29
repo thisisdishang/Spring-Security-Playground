a0a341eb76756a97fc3fb82a20cbf20feb11ec63
thisisdishang <dishangkumarrana@gmail.com>
2025-06-29 18:45:45 +0530

    Create user model

--- Authorize/src/main/java/com/ranacorporation/Authorize/service/UserService.java
+++ Authorize/src/main/java/com/ranacorporation/Authorize/service/UserService.java
@@ -1,9 +1,15 @@
 package com.ranacorporation.Authorize.service;
 
+import com.ranacorporation.Authorize.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
+
+    List<User> userList = new ArrayList();
 
     public void init() {
 
